79a917fbb642d585e87a2428ecf848586a059e54
James Pow <jbpow18@icloud.com>
2023-04-09 06:51:13 -0400

    feat: random updates

--- routes/root.js
+++ routes/root.js
@@ -2,15 +2,15 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 
-app.get('^/$|/index(.html)?', (req, res) => {
+router.get('^/$|/index(.html)?', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'views', 'index.html'));
 });
 
-app.get('/new-page(.html)?', (req, res) => {
+router.get('/new-page(.html)?', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'views', 'new-page.html'));
 });
 
-app.get('/old-page(.html)?', (req, res) => {
+router.get('/old-page(.html)?', (req, res) => {
   res.redirect(301, '/new-page.html'); // 302 by default
 });
 
