3c2d25b0344745691d410749c70114d15a4c049d
James Pow <jbpow18@icloud.com>
2023-04-09 07:22:58 -0400

    fix: clean up

--- server.js
+++ server.js
@@ -22,12 +22,10 @@ app.use(express.json());
 
 // serve static files
 app.use(express.static(path.join(__dirname, '/public')));
-app.use('/subdir', express.static(path.join(__dirname, '/public')));
-
-app.use('/subdir', require('./routes/subdir'));
 
 // routes
 app.use('/', require('./routes/root'));
+app.use('/employees', require('./routes/api/employees'));
 
 app.all('*', (req, res) => {
   res.status(404);
