a055f11044442fbb321eb40e2d9419a1c7574fab
James Pow <jbpow18@icloud.com>
2023-04-09 06:27:42 -0400

    feat: first commit

---
+++ routes/root.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+const path = require('path');
+
+app.get('^/$|/index(.html)?', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'views', 'index.html'));
+});
+
+app.get('/new-page(.html)?', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'views', 'new-page.html'));
+});
+
+app.get('/old-page(.html)?', (req, res) => {
+  res.redirect(301, '/new-page.html'); // 302 by default
+});
+
+module.exports = router;
